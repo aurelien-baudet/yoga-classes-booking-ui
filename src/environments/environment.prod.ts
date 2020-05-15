60d4a9cbe040b8f8c4b81024cf4495ca3f6adf03
Aurélien Baudet <aurelien.baudet@gmail.com>
2020-05-15 18:00:55 +0400

    chore(prod) Fix backend url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   mock: false,
   server: {
-    url: 'https://reservation.yogasaintpierre.fr/backend'
+    url: 'https://reservation.yogaterresainte.fr/backend'
   },
   onesignal: {
     appId: '2973b0ae-c479-481e-aa13-8a8239a05430',
