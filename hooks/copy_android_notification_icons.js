2bae9afd1fbbabc11f9a4d03abb585061eb50f0f
Aurélien Baudet <aurelien.baudet@gmail.com>
2020-06-16 09:58:06 +0400

    fix(android) Notification icons

--- hooks/copy_android_notification_icons.js
+++ hooks/copy_android_notification_icons.js
@@ -3,40 +3,51 @@
 var fs = require('fs');
 var path = require('path');
 
-var filestocopy = [{
-    "resources/android/icon/drawable-hdpi-icon.png":
-        "platforms/android/app/src/main/res/drawable-hdpi/ic_stat_onesignal_default.png"
+var rootDest = 'platforms/android/app/src/main/res';
+var files = [{
+  'resources/android/icon/hdpi-notification.png':
+     path.join(rootDest, 'drawable-hdpi/ic_stat_onesignal_default.png')
 }, {
-    "resources/android/icon/drawable-mdpi-icon.png":
-        "platforms/android/app/src/main/res/drawable-mdpi/ic_stat_onesignal_default.png"
+  'resources/android/icon/mdpi-notification.png':
+    path.join(rootDest, 'drawable-mdpi/ic_stat_onesignal_default.png')
 }, {
-    "resources/android/icon/drawable-xhdpi-icon.png":
-        "platforms/android/app/src/main/res/drawable-xhdpi/ic_stat_onesignal_default.png"
+  'resources/android/icon/xhdpi-notification.png':
+    path.join(rootDest, 'drawable-xhdpi/ic_stat_onesignal_default.png')
 }, {
-    "resources/android/icon/drawable-xxhdpi-icon.png":
-        "platforms/android/app/src/main/res/drawable-xxhdpi/ic_stat_onesignal_default.png"
+  'resources/android/icon/xxhdpi-notification.png':
+    path.join(rootDest, 'drawable-xxhdpi/ic_stat_onesignal_default.png')
 }, {
-    "resources/android/icon/drawable-xxxhdpi-icon.png":
-        "platforms/android/app/src/main/res/drawable-xxxhdpi/ic_stat_onesignal_default.png"
-} ];
+  'resources/android/icon/xxxhdpi-notification.png':
+    path.join(rootDest, 'drawable-xxxhdpi/ic_stat_onesignal_default.png')
+}];
+
+function createFolder(pathAbsolute) {
+  if (!fs.existsSync(pathAbsolute)) {
+    fs.mkdirSync(pathAbsolute);
+  }
+
+  console.log('Folder ready ', pathAbsolute);
+}
 
 module.exports = function(context) {
+  var root = context.opts.projectRoot;
 
-    // no need to configure below
-    var rootdir = context.opts.projectRoot;
+  createFolder(path.join(root, rootDest, 'drawable-hdpi'));
+  createFolder(path.join(root, rootDest, 'drawable-mdpi'));
+  createFolder(path.join(root, rootDest, 'drawable-xhdpi'));
+  createFolder(path.join(root, rootDest, 'drawable-xxhdpi'));
+  createFolder(path.join(root, rootDest, 'drawable-xxxhdpi'));
 
-    filestocopy.forEach(function(obj) {
-        Object.keys(obj).forEach(function(key) {
-            var val = obj[key];
-            var srcfile = path.join(rootdir, key);
-            var destfile = path.join(rootdir, val);
-            console.log("copying "+srcfile+" to "+destfile);
-            var destdir = path.dirname(destfile);
-            if (fs.existsSync(srcfile) && fs.existsSync(destdir)) {
-                fs.createReadStream(srcfile).pipe(
-                    fs.createWriteStream(destfile));
-            }
-        });
+  files.forEach(function(obj) {
+    Object.keys(obj).forEach(function(key) {
+      var src = path.join(root, key);
+      var dest = path.join(root, obj[key]);
+
+      if (fs.existsSync(src) && fs.existsSync(path.dirname(dest))) {
+        fs.createReadStream(src).pipe(fs.createWriteStream(dest));
+
+        console.log('Copied ' + src + ' to ' + dest);
+      }
     });
-
+  });
 };
