b3824d1abfe3ec410530937ffb92e97055384797
Aurélien Baudet <aurelien.baudet@gmail.com>
2020-06-17 17:03:07 +0400

    chore(travis) Prepare for production

--- generate-secrets.sh
+++ generate-secrets.sh
@@ -6,7 +6,7 @@ SECRETS_KEY="$1"
 #travis setup releases --pro --force
 
 rm -f secrets.tar.enc secrets.tar
-tar cvf secrets.tar -C ~/Documents/yoga-saint-pierre/travis-secrets debug.keystore
+tar cvf secrets.tar -C ~/Documents/yoga-saint-pierre/travis-secrets debug.keystore production.jks
 #travis login --pro --auto
 #travis encrypt-file secrets.tar --pro --add
 openssl enc -aes-256-cbc -e -in "secrets.tar" -out "secrets.tar.enc" -K $SECRETS_KEY -iv $SECRETS_IV
